41cf1b3fe19821f5a9b254872eb61d38cd5a44b7
Said Tahsin Dane <tasomaniac@gmail.com>
2015-04-14 21:06:43 +0300

    refactor

--- mobile/src/main/java/com/vngrs/android/pomodoro/receiver/PomodoroNotificationReceiver.java
+++ mobile/src/main/java/com/vngrs/android/pomodoro/receiver/PomodoroNotificationReceiver.java
@@ -1,4 +1,4 @@
-package com.vngrs.android.pomodoro.receivers;
+package com.vngrs.android.pomodoro.receiver;
 
 import android.content.Context;
 import android.content.Intent;
@@ -9,9 +9,9 @@ import com.vngrs.android.pomodoro.shared.BaseNotificationReceiver;
 /**
  * Created by Said Tahsin Dane on 02/04/15.
  */
-public class PomodoroReceiver extends BaseNotificationReceiver {
+public class PomodoroNotificationReceiver extends BaseNotificationReceiver {
 
-    public PomodoroReceiver() {
+    public PomodoroNotificationReceiver() {
     }
 
     @Override
